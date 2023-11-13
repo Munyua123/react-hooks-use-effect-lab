4eab51005884b77b017d33057adb2aa819d4281b
Earl <earlmunyua96@gmail.com>
2023-11-13 18:13:03 +0300

    ueEffect

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,28 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+
+  useEffect(() => {
+    // Set up a countdown
+    const countdown = () => {
+      if (timeRemaining > 0) {
+        setTimeRemaining(timeRemaining - 1);
+      } else {
+        setTimeRemaining(10);
+        onAnswered(false); // Call onAnswered with false when time runs out
+      }
+    };
+
+    const timer = setInterval(countdown, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [timeRemaining, onAnswered]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
